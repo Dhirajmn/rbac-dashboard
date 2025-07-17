eb7e52c8a11c09c26ace5b6a000893de1f6051a9
Dhiraj Nagar <Dhirajnagar2003@gmail.com>
2025-07-17 20:10:35 +0530

    Initial commit

---
+++ src/main/java/com/dhiraj/rbacdashboard/RbacDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.dhiraj.rbacdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RbacDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dhiraj/rbacdashboard/RbacDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhiraj.rbacdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RbacDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
